783d43db503b0c4eb93434b4ca72e549514752f6
VinceMaestro <vpetit@e3r5p8.42.fr>
2017-10-25 15:56:33 +0200

    structuring code

--- push_swap.c
+++ push_swap.c
@@ -6,13 +6,13 @@
 /*   By: vpetit <vpetit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/25 14:56:11 by vpetit            #+#    #+#             */
-/*   Updated: 2017/10/25 15:06:52 by vpetit           ###   ########.fr       */
+/*   Updated: 2017/10/25 15:46:42 by vpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	merge_halves(int ar_a, int left_start, int right_end)
+static void	merge_halves(int **ar_a, int **ar_b, int left_start, int right_end)
 {
 	int		left_end;
 	int		right_start;
@@ -24,17 +24,17 @@ static void	merge_halves(int ar_a, int left_start, int right_end)
 	while (left <= left_end && right <= right_end)
 	{
 		if (ar_a[left] <= ar_a[right])
-			temp[j] = ar_a[left++];
+			ar_b[j] = ar_a[left++];
 		else
-			temp[j] = ar_a[right++];
+			ar_b[j] = ar_a[right++];
 		j++;
 	}
-	cpfrom_a_i_to_b_j_nelem(ar_a, left, tmp, j, left_end - left + 1);
-	cpfrom_a_i_to_b_j_nelem(ar_a, right, tmp, j, right_end - right + 1);
-	cpfrom_a_i_to_b_j_nelem(tmp, left_start, ar_a, left_start, len);
+	cpfrom_a_i_to_b_j_nelem(ar_a, left, ar_b, j, left_end - left + 1);
+	cpfrom_a_i_to_b_j_nelem(ar_a, right, ar_b, j, right_end - right + 1);
+	cpfrom_a_i_to_b_j_nelem(ar_b, left_start, ar_a, left_start, len);
 }
 
-static void	merge_sort(int **ar_a, int **ar_a, int left_start, int right_end)
+static void	merge_sort(int **ar_a, int **ar_b, int left_start, int right_end)
 {
 	int		middle;
 
@@ -48,17 +48,13 @@ static void	merge_sort(int **ar_a, int **ar_a, int left_start, int right_end)
 
 static void	ft_main(char **str_ar)
 {
-	int		**ar_a;
-	int		**ar_b;
+	t_arr	arr;
 	int		len;
 
 	if ((len = ft_to_int_ar(str_ar, ar_a)) == -1)
-		fr_error("Error\n")
-
-	ar_b = ft_new
-	// creer ar_a int**
-	// creer ar_b int** of size ar_a.lenght
-	merge_sort(ar_a, ar_b 0, len)
+		fr_error("Error\n");
+	ar_b = ft_new_int_ar(len);
+	merge_sort(ar_a, ar_b 0, len);
 }
 
 int     	main(int argc, char **argv)
--- push_swap.h
+++ push_swap.h
@@ -4,17 +4,32 @@
 #include "libft/libft.h"
 #include "get_next_line.h"
 
-# define F_NONE (1 << 0)
-# define F_SA   (1 << 1)
-# define F_SB   (1 << 2)
-# define F_SS   (1 << 3)
-# define F_PA   (1 << 4)
-# define F_PB   (1 << 5)
-# define F_RA   (1 << 6)
-# define F_RB   (1 << 7)
-# define F_RR   (1 << 8)
-# define F_RRA  (1 << 9)
-# define F_RRB  (1 << 10)
-# define F_RRR  (1 << 11)
+# define OP_NONE (1 << 0)
+# define OP_SA   (1 << 1)
+# define OP_SB   (1 << 2)
+# define OP_SS   (1 << 3)
+# define OP_PA   (1 << 4)
+# define OP_PB   (1 << 5)
+# define OP_RA   (1 << 6)
+# define OP_RB   (1 << 7)
+# define OP_RR   (1 << 8)
+# define OP_RRA  (1 << 9)
+# define OP_RRB  (1 << 10)
+# define OP_RRR  (1 << 11)
+
+# define AR_A	arr->arr_a;
+# define AR_B	arr->arr_b;
+# define AR_LN	arr->arr_len;
+# define AR_OP	arr->op;
+
+typedef struct s_arr	t_arr;
+
+struct s_arr
+{
+	int		*arr_a;
+	int		*arr_b;
+	int		arr_len;
+	int		*op;
+};
 
 #endif
