e8d5adf3d3838e7b336b4a090990ddc1b6a2f413
VinceMaestro <vpetit@e3r5p8.42.fr>
2017-10-25 19:55:22 +0200

    working on algo

--- checker.c
+++ checker.c
@@ -19,3 +19,33 @@ int     main(int argc, char **argv)
 		ft_error("Error\n");
     return (0);
 }
+
+
+checker :
+prends une liste d instructions & une liste d entiers sur la stdout
+creer une deuxieme liste d entiers
+execute les instructions sur la liste
+verifie si la liste est triée
+
+push_swap :
+prends une liste de str,
+la transforme en entiers
+creer deux listes d entiers
+applique un algorithme de tri sur cette liste
+	execute les instructions de l algo sur la liste
+	enregistre les instructions effectuees sur la liste
+
+2 modes :	M_CHECK
+			M_PUSH
+// box, inndependante, prends des inputs de checker et push_swap
+
+exec(int instruction, t_arr *arr)
+	
+
+box(t_arr *arr, int *instruction(only 4 M_PUSH), int mode(M_CHECK ou M_PUSH))
+	if (!((mode & M_CHECK) || (mode & M_PUSH)))
+		ft_error("Error : invalid mode\n");
+	while (instruction[i])
+		exec(instruction[i], arr);
+		i++;
+	return (mode == M_PUSH ? push(instruction, arr) : verif_order(arr));
--- push_swap.h
+++ push_swap.h
@@ -17,12 +17,19 @@
 # define OP_RRB  (1 << 10)
 # define OP_RRR  (1 << 11)
 
+# define M_CHECK	0
+# define M_PUSH		1
+
+# define VALID		0
+# define INVALID	-1
+
 # define AR_A	arr->arr_a;
 # define AR_B	arr->arr_b;
 # define AR_LN	arr->arr_len;
 # define AR_OP	arr->op;
 
 typedef struct s_arr	t_arr;
+typedef struct s_fptr	t_fptr;
 
 struct s_arr
 {
@@ -32,4 +39,27 @@ struct s_arr
 	int		*op;
 };
 
+struct s_fptr
+{
+	int(*ft_ptr_fct)()
+};
+
 #endif
+
+static		void (*(g_ft_ptr_fct[]))(t_a_lst *, t_f_id *) =
+{
+	['s'] = ft_print_a_s,
+	['S'] = ft_print_a_s,
+	['p'] = ft_print_a_p,
+	['d'] = ft_print_a_i,
+	['D'] = ft_print_a_i,
+	['i'] = ft_print_a_i,
+	['o'] = ft_print_a_o,
+	['O'] = ft_print_a_o,
+	['u'] = ft_print_a_u,
+	['U'] = ft_print_a_u,
+	['x'] = ft_print_a_x,
+	['X'] = ft_print_a_x,
+	['c'] = ft_print_a_c,
+	['C'] = ft_print_a_c
+};
